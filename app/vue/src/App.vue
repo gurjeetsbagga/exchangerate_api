d72960e132a5667d582f32d069dbf8305576c790
Gurjeet Singh <gurjeetsbagga@gmail.com>
2022-10-10 22:58:34 +0530

    first working api fix

--- app/vue/src/App.vue
+++ app/vue/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ExchangeRate/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExchangeRate from './components/ExchangeRate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ExchangeRate
   }
 }
 </script>
---
+++ app/vue/src/components/ExchangeRate.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="m-auto">
+    <h1>Currency Converter</h1>
+    <section v-if="errored">
+      <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">Loading...</div>
+      <div v-else
+          class="currency">
+        <div class="box">
+        <span class="lighten">
+          <table class="table-auto">
+            <tbody>
+            <tr>
+              <td></td>
+              <td><strong>1 {{info.base_code}} = </strong></td>
+            </tr>
+            <tr v-for="(key, value) in codes" :key="key">
+                <td>{{Countries[value]}}</td>
+                <td>{{key}}</td>
+            </tr>
+            <tr>
+              <td>
+              </td>
+              <td>
+                <button @click='reload' class="bg-red-100 px-4 py-3 hover:bg-red-300">Refresh</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Rates {{currentDate()}}</td>
+            </tr>
+            </tbody>
+          </table>
+        </span>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import '../assets/index.css';
+
+export default {
+  name: "ExchangeRate",
+  data() {
+    return {
+      Countries : {
+        "EUR":"EURO",
+        "GBP":"British Pound Sterling",
+        "JPY":"Japanese Yen",
+        "CHF":"Swiss Franc",
+        "CAD":"Canadian Dollar",
+        "AUD":"Australian Dollar",
+        "CNY":"Chinese Yuan Renminbi",
+        "ZAR":"South African Rand",
+        "BRL":"Brazilian Real",
+        "HKD":"Hong Kong Dollar",
+        "MXN":"Mexican Peso"},
+      info: null,
+      codes: null,
+      loading: false,
+      errored: false
+    };
+  },
+  methods:{
+    reload() {
+      location.reload();
+    },
+    currentDate() {
+      const current = new Date();
+      const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+      const date = `${current.getDate()} ${month[current.getMonth()]} ${current.getFullYear()}`;
+      return date;
+    }
+  },
+  created() {
+    axios.get('http://localhost:82/api/exchange_rate/usd')
+        .then(response => {
+          this.info = response.data;
+          var items = {};
+          Object.keys(this.info.conversion_rates).forEach((value) => {
+            if (Object.keys(this.Countries).includes(value)) {
+              items[value] = this.info.conversion_rates[value];
+              }
+          });
+          this.codes = items;
+          console.log(this.codes)
+          this.errored=false;
+        })
+        .catch(e => {
+          this.errored=true;
+          console.log(e)
+          this.errors.push(e)
+        })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
